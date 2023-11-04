9650822ca645cdf3f439b19e5a82bf9398e81598
schmidtdennis <bgk.dennis@yahoo.de>
2023-11-04 22:11:34 +0100

    added side menu

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,14 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MenuComponent } from './components/menu/menu.component';
+import { MatIconModule } from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
+import { RouterModule } from '@angular/router';
 
 
 @NgModule({
   declarations: [
+    MenuComponent
   ],
-  exports: [],
+  exports: [MenuComponent],
   imports: [
     CommonModule,
-    
+    MatIconModule,
+    MatListModule,
+    MatDividerModule,
+    RouterModule,
   ]
 })
 export class CoreModule { }
