99ab34180b0b9ed5e016f85c02d62a3901664c7b
schmidtdennis <bgk.dennis@yahoo.de>
2023-11-05 14:47:36 +0100

    finished sidenavbar

--- src/app/core/components/menu/menu.component.ts
+++ src/app/core/components/menu/menu.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
-export class MenuComponent {
-  send(){
-    console.log("hi");  
+export class MenuComponent implements OnInit, AfterViewInit {
+
+  @Input('sidenav') sidenav!: MatSidenav;
+
+  constructor(private router: Router) { }
+
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    
+  }
+
+  navigateTo(target: string){
+    this.router.navigate([target]);
+    this.sidenav.toggle();
   }
 }
