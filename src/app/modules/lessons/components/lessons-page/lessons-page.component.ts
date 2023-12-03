5b06301dea6344824f7ad654f35703d3faf634c2
schmidtdennis <bgk.dennis@yahoo.de>
2023-12-03 08:53:02 +0100

    added environments

--- src/app/modules/lessons/components/lessons-page/lessons-page.component.ts
+++ src/app/modules/lessons/components/lessons-page/lessons-page.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-lessons-page',
   templateUrl: './lessons-page.component.html',
   styleUrls: ['./lessons-page.component.scss']
 })
-export class LessonsPageComponent {
+export class LessonsPageComponent implements OnInit {
+
   lessons: any[] = [
     {
       title: "First Lesson",
@@ -23,4 +25,13 @@ export class LessonsPageComponent {
       imageUrl: "https://material.angular.io/assets/img/examples/shiba2.jpg"
     },
   ]
+
+  ngOnInit(): void {
+    // get lessons from backend
+    console.log('backend-url: ' + environment.apiUrl);
+    console.log('Role: ' + process.env["CURRENT_ROLE"]);
+
+  }
+
+  
 }
